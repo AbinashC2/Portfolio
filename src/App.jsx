3d6a27431cf34fb23fcc82e82666ae9dd9c9254c
AbinashC2 <abinashmohapatra0007@gmail.com>
2025-02-01 23:53:27 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import HashLoader from "react-spinners/HashLoader";
-import ContactMe from "./components/Contactme";
+import ContactMe from "./components/ContactMe";
+
 import { useState, useEffect } from "react";
 
 
